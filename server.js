bccdd0ef022cc925ce34aad91888899c6bec1167
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2022-08-22 11:32:28 +0300

    Remove irrelevant comments

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var { graphql, buildSchema } = require('graphql');
 
-// Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type result {
     arg1: Float!
@@ -11,14 +10,12 @@ var schema = buildSchema(`
   }
 `);
 
-// The rootValue provides a resolver function for each API endpoint
 var rootValue = {
   test: (args) => {
     return args;
   },
 };
 
-// Run the GraphQL query '{ hello }' and print out the response
 graphql({
   schema,
   source: '{ test(arg1: 1.1111111)  { arg1 } }',
