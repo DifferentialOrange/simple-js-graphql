c55f3df79b4807d8cc46d12c91f98964bebc49b8
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2022-08-22 18:07:24 +0300

    WIP defaults: inconsistency with Lua

--- server.js
+++ server.js
@@ -18,6 +18,24 @@ function PrintResponse(response) {
   }
 }
 
+var box = {NULL: 'null'}
+
+function transform_value(v) {
+  if (v === box.NULL) {
+    return null
+  }
+
+  if (Array.isArray(v) && v.length > 0) {
+    if (v[0] === box.NULL) {
+      return [null]
+    } else if (v[0] === null) { // Lua {nil} is {}
+      return []
+    }
+  }
+
+  return v
+};
+
 function BuildTestCase(argument_type, argument_nullability,
                        argument_inner_type, argument_inner_nullability,
                        value,
@@ -26,21 +44,17 @@ function BuildTestCase(argument_type, argument_nullability,
                        error_msg) {
   var argument_str
 
-  var argument_nullability_str
+  var argument_nullability_str = ``
   if (argument_nullability) {
     argument_nullability_str = `!`
-  } else {
-    argument_nullability_str = ``
   }
 
-  var argument_inner_nullability_str
+  var argument_inner_nullability_str = ``
   if (argument_inner_nullability) {
     argument_inner_nullability_str = `!`
-  } else {
-    argument_inner_nullability_str = ``
   }
 
-  if (argument_type == 'list') {
+  if (argument_type === 'list') {
     argument_str = `[${argument_inner_type}${argument_inner_nullability_str}]${argument_nullability_str}`
   } else {
     argument_str = `${argument_type}${argument_nullability_str}`
@@ -58,47 +72,60 @@ function BuildTestCase(argument_type, argument_nullability,
 
   var schema = buildSchema(schema_str);
 
-  var value_str = JSON.stringify(value)
-
+  var value_str = `null`
+  if ((value !== null) && (value !== box.NULL)) {
+    value_str = JSON.stringify(transform_value(value))
+  }
+   
   var query
   var variables = []
-  if (variable_nullability != null) {
+  if (variable_nullability !== null) {
     var variable_str
 
-    var variable_nullability_str
+    var variable_nullability_str = ``
     if (variable_nullability) {
       variable_nullability_str = `!`
-    } else {
-      variable_nullability_str = ``
     }
 
-    var variable_inner_nullability_str
+    var variable_inner_nullability_str = ``
     if (variable_inner_nullability) {
       variable_inner_nullability_str = `!`
-    } else {
-      variable_inner_nullability_str = ``
     }
 
-    if (argument_type == 'list') {
+    if (argument_type === 'list') {
       variable_str = `[${variable_inner_type}${variable_inner_nullability_str}]${variable_nullability_str}`
     } else {
       variable_str = `${argument_type}${variable_nullability_str}`
     }
 
-    var default_str
-    if (default_variable_value != null) {
-      default_str = ` = ${default_variable_value}`
+    var default_str = ``
+    if (default_variable_value !== null) {
+      var def_prestr = transform_value(default_variable_value)
+      default_str = ` = ${def_prestr}`
     }
 
     query = `query MyQuery($var1: ${variable_str}${default_str}) { test(arg1: $var1) { arg1 } }`
-    variables = {var1: value}
+
+    if (value !== null) {
+      if (value === box.NULL) {
+        variables = {var1: null}
+      } else {
+        variables = {var1: value}
+      }
+    }
   } else {
     query = `query MyQuery { test(arg1: ${value_str}) { arg1 } }`
   }
 
-  // console.log(schema_str);
-  // console.log(query);
-  // console.log(variables);
+  console.log(schema_str);
+  console.log(value);
+  // console.log(value !== null);
+  // console.log(value !== box.NULL);
+  // console.log(value.hasOwnProperty('isArray'));
+  // console.log(value.isArray());
+  // console.log(value.length);
+  console.log(query);
+  console.log(variables);
 
   return graphql({
     schema,
@@ -106,15 +133,22 @@ function BuildTestCase(argument_type, argument_nullability,
     rootValue,
     variableValues: variables
   }).then((response) => {
-    if (error_msg != null) {
+    if (error_msg !== null) {
+      if (response.hasOwnProperty('data')) {
+        console.log(JSON.parse(JSON.stringify(response.data)))
+        test.fail('Data got when error expected')
+      }
       test.assert.deepEqual(response.errors[0].message, error_msg)
     } else {
-      test.assert.equal(response.hasOwnProperty('errors'), false, response.errors)
+      if (response.hasOwnProperty('errors')) {
+        console.log(JSON.parse(JSON.stringify(response.errors)))
+        test.fail('Errors got when data expected')
+      }
 
-      if ((default_variable_value != null) && (value == null)) {
-        test.assert.deepEqual(response.data.test.arg1, default_variable_value)
+      if ((default_variable_value !== null) && (value === null)) {
+        test.assert.deepEqual(response.data.test.arg1, transform_value(default_variable_value))
       } else {
-        test.assert.deepEqual(response.data.test.arg1, value)
+        test.assert.deepEqual(response.data.test.arg1, transform_value(value))
       }
     }
   });
@@ -124,7 +158,6 @@ var k = 'Float'
 var Nullable = false
 var NonNullable = true
 var v = {value: 1.1111111, default: 0}
-var box = {NULL: null}
 var nil = null
 
 describe('test_nonlist_arguments_nullability', function() {
@@ -203,7 +236,7 @@ describe('test_list_arguments_nullability', function() {
   });
 
   it('(13) Argument: [T]! -> Value: [null] - OK', async function() {
-    await BuildTestCase('list', NonNullable, k, Nullable, [nil], nil, nil, nil, nil, nil);
+    await BuildTestCase('list', NonNullable, k, Nullable, [box.NULL], nil, nil, nil, nil, nil);
   });
 
   it('(14) Argument: [T]! -> Value: nil - FAIL', async function() {
@@ -236,7 +269,147 @@ describe('test_list_arguments_nullability', function() {
 });
 
 describe('test_nonlist_arguments_with_variables_nullability', function() {
-  it('(1) Argument: T -> Value: value - OK', async function() {
+  it('(1) Argument: T -> Variable: T -> Value: value -> Default: value - OK', async function() {
     await BuildTestCase(k, Nullable, nil, nil, v.value, Nullable, nil, nil, v.default, nil);
+  });
+
+  it('(2) Argument: T -> Variable: T -> Value: value -> Default: nil - OK', async function() {
+    await BuildTestCase(k, Nullable, nil, nil, v.value, Nullable, nil, nil, nil, nil);
+  });
+
+  it('(3) Argument: T -> Variable: T -> Value: value -> Default: null - OK', async function() {
+    await BuildTestCase(k, Nullable, nil, nil, v.value, Nullable, nil, nil, box.NULL, nil);
+  });
+
+  it('(4) Argument: T -> Variable: T -> Value: nil -> Default: value - OK', async function() {
+    await BuildTestCase(k, Nullable, nil, nil, nil, Nullable, nil, nil, v.default, nil);
+  });
+
+  it('(5) Argument: T -> Variable: T -> Value: nil -> Default: nil - OK', async function() {
+    await BuildTestCase(k, Nullable, nil, nil, nil, Nullable, nil, nil, nil, nil);
+  });
+
+  it('(6) Argument: T -> Variable: T -> Value: nil -> Default: null - OK', async function() {
+    await BuildTestCase(k, Nullable, nil, nil, nil, Nullable, nil, nil, box.NULL, nil);
+  });
+
+  it('(7) Argument: T -> Variable: T -> Value: null -> Default: value - OK', async function() {
+    await BuildTestCase(k, Nullable, nil, nil, box.NULL, Nullable, nil, nil, v.default, nil);
+  });
+
+  it('(8) Argument: T -> Variable: T -> Value: null -> Default: nil - OK', async function() {
+    await BuildTestCase(k, Nullable, nil, nil, box.NULL, Nullable, nil, nil, nil, nil);
+  });
+
+  it('(9) Argument: T -> Variable: T -> Value: null -> Default: null - OK', async function() {
+    await BuildTestCase(k, Nullable, nil, nil, box.NULL, Nullable, nil, nil, box.NULL, nil);
+  });
+
+  it('(10) Argument: T -> Variable: T! -> Value: value -> Default: value - FAIL', async function() {
+    await BuildTestCase(k, Nullable, nil, nil, v.value, NonNullable, nil, nil, v.default, 'Non-null variables can not have default values');
+  });
+
+  it('(11) Argument: T -> Variable: T! -> Value: value -> Default: nil - OK', async function() {
+    await BuildTestCase(k, Nullable, nil, nil, v.value, NonNullable, nil, nil, nil, nil);
+  });
+
+  it('(12) Argument: T -> Variable: T! -> Value: value -> Default: null - FAIL', async function() {
+    await BuildTestCase(k, Nullable, nil, nil, v.value, NonNullable, nil, nil, box.NULL, 'Non-null variables can not have default values');
+  });
+
+  it('(13) Argument: T -> Variable: T! -> Value: nil -> Default: value - FAIL', async function() {
+    await BuildTestCase(k, Nullable, nil, nil, nil, NonNullable, nil, nil, v.default, 'Non-null variables can not have default values');
+  });
+
+  it('(14) Argument: T -> Variable: T! -> Value: nil -> Default: nil - FAIL', async function() {
+    await BuildTestCase(k, Nullable, nil, nil, nil, NonNullable, nil, nil, nil, 'Variable "var1" expected to be non-null');
+  });
+
+  it('(15) Argument: T -> Variable: T! -> Value: nil -> Default: null - FAIL', async function() {
+    await BuildTestCase(k, Nullable, nil, nil, nil, NonNullable, nil, nil, box.NULL, 'Non-null variables can not have default values');
+  });
+
+  it('(16) Argument: T -> Variable: T! -> Value: null -> Default: value - FAIL', async function() {
+    await BuildTestCase(k, Nullable, nil, nil, box.NULL, NonNullable, nil, nil, v.default, 'Non-null variables can not have default values');
+  });
+
+  it('(17) Argument: T -> Variable: T! -> Value: null -> Default: nil - FAIL', async function() {
+    await BuildTestCase(k, Nullable, nil, nil, box.NULL, NonNullable, nil, nil, nil, 'Variable "var1" expected to be non-null');
+  });
+
+  it('(18) Argument: T -> Variable: T! -> Value: null -> Default: null - FAIL', async function() {
+    await BuildTestCase(k, Nullable, nil, nil, box.NULL, NonNullable, nil, nil, box.NULL, 'Non-null variables can not have default values');
+  });
+
+  it('(19) Argument: T! -> Variable: T -> Value: value -> Default: value - OK', async function() {
+    await BuildTestCase(k, NonNullable, nil, nil, v.value, Nullable, nil, nil, v.default, nil);
+  });
+
+  it('(20) Argument: T! -> Variable: T -> Value: value -> Default: nil - FAIL', async function() {
+    await BuildTestCase(k, NonNullable, nil, nil, v.value, Nullable, nil, nil, nil, 'Variable "var1" type mismatch: the variable type "" is not compatible with the argument type "NonNull()"');
+  });
+
+  it('(21) Argument: T! -> Variable: T -> Value: value -> Default: null - OK', async function() {
+    await BuildTestCase(k, NonNullable, nil, nil, v.value, Nullable, nil, nil, box.NULL, nil);
+  });
+
+  it('(22) Argument: T! -> Variable: T -> Value: nil -> Default: value - OK', async function() {
+    await BuildTestCase(k, NonNullable, nil, nil, nil, Nullable, nil, nil, v.default, nil);
+  });
+
+  it('(23) Argument: T! -> Variable: T -> Value: nil -> Default: nil - FAIL', async function() {
+    await BuildTestCase(k, NonNullable, nil, nil, nil, Nullable, nil, nil, nil, 'Variable "var1" type mismatch: the variable type ""is not compatible with the argument type "NonNull()"');
+  });
+
+  it('(24) Argument: T! -> Variable: T -> Value: nil -> Default: null - FAIL', async function() {
+    await BuildTestCase(k, NonNullable, nil, nil, nil, Nullable, nil, nil, box.NULL, 'Expected non-null for "NonNull()", got null');
+  });
+
+  it('(25) Argument: T! -> Variable: T -> Value: null -> Default: value - FAIL', async function() {
+    await BuildTestCase(k, NonNullable, nil, nil, box.NULL, Nullable, nil, nil, v.default, 'Expected non-null for "NonNull()", got null');
+  });
+
+  it('(26) Argument: T! -> Variable: T -> Value: null -> Default: nil - FAIL', async function() {
+    await BuildTestCase(k, NonNullable, nil, nil, box.NULL, Nullable, nil, nil, nil, 'Variable "var1" type mismatch: the variable type ""is not compatible with the argument type "NonNull()"');
+  });
+
+  it('(27) Argument: T! -> Variable: T -> Value: null -> Default: null - FAIL', async function() {
+    await BuildTestCase(k, NonNullable, nil, nil, box.NULL, Nullable, nil, nil, box.NULL, 'Expected non-null for "NonNull()", got null');
+  });
+
+  it('(28) Argument: T! -> Variable: T! -> Value: value -> Default: value - FAIL', async function() {
+    await BuildTestCase(k, NonNullable, nil, nil, v.value, NonNullable, nil, nil, v.default, 'Non-null variables can not have default values');
+  });
+
+  it('(29) Argument: T! -> Variable: T! -> Value: value -> Default: nil - OK', async function() {
+    await BuildTestCase(k, NonNullable, nil, nil, v.value, NonNullable, nil, nil, nil, nil);
+  });
+
+  it('(30) Argument: T! -> Variable: T! -> Value: value -> Default: null - FAIL', async function() {
+    await BuildTestCase(k, NonNullable, nil, nil, v.value, NonNullable, nil, nil, box.NULL, 'Non-null variables can not have default values');
+  });
+
+  it('(31) Argument: T! -> Variable: T! -> Value: nil -> Default: value - FAIL', async function() {
+    await BuildTestCase(k, NonNullable, nil, nil, nil, NonNullable, nil, nil, v.default, 'Non-null variables can not have default values');
+  });
+
+  it('(32) Argument: T! -> Variable: T! -> Value: nil -> Default: nil - FAIL', async function() {
+    await BuildTestCase(k, NonNullable, nil, nil, nil, NonNullable, nil, nil, nil, 'Variable "var1" expected to be non-null');
+  });
+
+  it('(33) Argument: T! -> Variable: T! -> Value: nil -> Default: null - FAIL', async function() {
+    await BuildTestCase(k, NonNullable, nil, nil, nil, NonNullable, nil, nil, box.NULL, 'Non-null variables can not have default values');
+  });
+
+  it('(34) Argument: T! -> Variable: T! -> Value: null -> Default: value - FAIL', async function() {
+    await BuildTestCase(k, NonNullable, nil, nil, box.NULL, NonNullable, nil, nil, v.default, 'Non-null variables can not have default values');
+  });
+
+  it('(35) Argument: T! -> Variable: T! -> Value: null -> Default: nil - FAIL', async function() {
+    await BuildTestCase(k, NonNullable, nil, nil, box.NULL, NonNullable, nil, nil, nil, 'Variable "var1" expected to be non-null');
+  });
+
+  it('(36) Argument: T! -> Variable: T! -> Value: null -> Default: null - FAIL', async function() {
+    await BuildTestCase(k, NonNullable, nil, nil, box.NULL, NonNullable, nil, nil, box.NULL, 'Non-null variables can not have default values');
   });
 });
